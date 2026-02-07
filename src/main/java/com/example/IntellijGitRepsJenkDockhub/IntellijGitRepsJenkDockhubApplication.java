64bcf509f3892e9b3bf05e403dfeeebab0132a24
mdamirhussain <amir4067@gmail.com>
2026-02-07 19:00:15 +0530

    initial commit

---
+++ src/main/java/com/example/IntellijGitRepsJenkDockhub/IntellijGitRepsJenkDockhubApplication.java
@@ -0,0 +1,15 @@
+package com.example.IntellijGitRepsJenkDockhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijGitRepsJenkDockhubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntellijGitRepsJenkDockhubApplication.class, args);
+		System.out.println("Testing ......Amir");
+	}
+
+}
---
+++ src/test/java/com/example/IntellijGitRepsJenkDockhub/IntellijGitRepsJenkDockhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntellijGitRepsJenkDockhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijGitRepsJenkDockhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
